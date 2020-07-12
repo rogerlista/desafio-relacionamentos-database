55282fa01042733a12bad11e730fd45dd9312dd7
Rogério <roger.lista@gmail.com>
2020-07-12 20:39:07 -0300

    Desafio 09 - rota customers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,4 +9,7 @@ import ProductsRepository from '@modules/products/infra/typeorm/repositories/Pro
 import IOrdersRepository from '@modules/orders/repositories/IOrdersRepository';
 import OrdersRepository from '@modules/orders/infra/typeorm/repositories/OrdersRepository';
 
-// TODO
+container.registerSingleton<ICustomersRepository>(
+  'CustomersRepository',
+  CustomersRepository,
+);
